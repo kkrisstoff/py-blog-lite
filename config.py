afa49c9ab7bdce71c6ca497398d74c9c72a5cec2
Kostya <pkostyan2007@gmail.com>
2020-02-05 21:43:43 +0200

    heroku deployment changes

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     LANGUAGES = ['en', 'ru']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 5
 
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
